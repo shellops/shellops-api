0b10e40deb71d47e0d0c60c40bf95d1b6813348f
Mohsen Esmaeili <m-esm@hotmail.com>
2021-01-16 01:09:39 +0330

    feat: location map

--- src/sysinfo/sysinfo.controller.ts
+++ src/sysinfo/sysinfo.controller.ts
@@ -1,6 +1,10 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 
 import { SysinfoService } from './sysinfo.service';
+
+import * as svgMap from 'svg-world-map';
+import * as domStringify from 'virtual-dom-stringify';
+import { Response } from 'express';
 
 @Controller()
 export class SysinfoController {
@@ -10,25 +14,43 @@ export class SysinfoController {
     }
 
     @Get('/api/v1/sysinfo/:node/general')
-    general() {
+    async general() {
         return this.sysInfoService.general();
     }
 
 
     @Get('/api/v1/sysinfo/:node/ip')
-    ip() {
+    async ip() {
         return this.sysInfoService.ip();
     }
 
 
     @Get('/api/v1/sysinfo/:node/geo-ip')
-    geoIp() {
+    async geoIp() {
         return this.sysInfoService.geoIp();
+    }
+
+    @Get('/api/v1/sysinfo/:node/location.svg')
+    async geoMap(@Res() res: Response) {
+
+        const { lon, lat } = await this.sysInfoService.geoIp();
+
+        res.setHeader('Content-Type', 'image/svg+xml');
+
+        const map = svgMap(lon, lat, {
+            ocean: 'transparent', // color of the ocean
+            land: '#ccc', // color of the land
+            mapWidth: 500, // width of the `<svg>`
+            pinHeight: 4 // relative to map viewBox
+        });
+
+        res.end(domStringify(map));
+
     }
 
 
     @Get('/api/v1/sysinfo/:node/docker')
-    docker() {
+    async docker() {
         return this.sysInfoService.docker();
     }
 
