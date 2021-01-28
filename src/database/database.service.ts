85a643e4b35a684999afcdbf5c7961f377be0c76
Mohsen Esmaeili <m-esm@hotmail.com>
2021-01-29 00:57:21 +0330

    feat: persist db for agent mode

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import * as fbAdmin from 'firebase-admin';
-import * as _ from 'lodash'
+import { readJsonSync, writeJson } from 'fs-extra';
+import * as _ from 'lodash';
+import { homedir } from 'os';
+import { join } from 'path';
+
 @Injectable()
 export class DatabaseService {
 
     firebase: fbAdmin.app.App;
-    temporary: any = {};
+    jsonPath = join(homedir(), '.shellops.json');
+    json: any = readJsonSync(this.jsonPath);
 
     constructor() { }
 
@@ -13,13 +18,18 @@ export class DatabaseService {
         if (this.firebase)
             return (await this.firebase.database().ref(path).get()).toJSON() as T;
         else
-            _.get(this.temporary, path);
+            _.get(this.json, path);
     }
 
     async update(path: string, update: any): Promise<void> {
         if (this.firebase)
             await this.firebase.database().ref(path).update(update);
-        else
-            _.set(this.temporary, path, _.extend(this.get(path), update));
+        else {
+            _.set(this.json, path, _.extend(this.get(path), update));
+
+            await writeJson(this.jsonPath, this.json, { spaces: 2 });
+        }
+
+
     }
 }
