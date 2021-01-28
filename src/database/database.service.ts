049cbc404f4713e58af4b2331cb5bb3a429cc80d
Mohsen Esmaeili <m-esm@hotmail.com>
2021-01-28 22:46:54 +0330

    feat: database service with temp data support

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,17 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import * as fbAdmin from 'firebase-admin';
-
+import * as _ from 'lodash'
 @Injectable()
 export class DatabaseService {
-  firebase = fbAdmin.initializeApp({});
 
-  constructor() {}
+    firebase: fbAdmin.app.App;
+    temporary: any = {};
 
-  async get(path: string) {
-    return (await this.firebase.database().ref(path).get()).toJSON();
-  }
+    constructor() { }
 
-  async update(path: string, update: any) {
-    await this.firebase.database().ref(path).update(update);
-  }
+    async get(path: string) {
+        if (this.firebase)
+            return (await this.firebase.database().ref(path).get()).toJSON();
+        else
+            _.get(this.temporary, path);
+    }
+
+    async update(path: string, update: any) {
+        if (this.firebase)
+            await this.firebase.database().ref(path).update(update);
+        else
+            _.set(this.temporary, path, _.extend(this.get(path), update));
+    }
 }
