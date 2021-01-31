e72d37034c742721c632e8331504b5aabeb0bc16
Mohsen Esmaeili <m-esm@hotmail.com>
2021-01-31 23:21:04 +0330

    fix: app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
+
 import { AppController } from './app.controller';
-import { SysinfoModule } from './sysinfo/sysinfo.module';
-import { WsModule } from './ws/ws.module';
+import { DatabaseModule } from './database/database.module';
+import { DockerModule } from './docker/docker.module';
+import { FinancialModule } from './financial/financial.module';
 import { MachineModule } from './machine/machine.module';
 import { StoreModule } from './store/store.module';
-import { FinancialModule } from './financial/financial.module';
-import { DockerModule } from './docker/docker.module';
-import { DatabaseModule } from './database/database.module';
+import { SysinfoModule } from './sysinfo/sysinfo.module';
+import { WsModule } from './ws/ws.module';
 
 @Module({
   imports: [
