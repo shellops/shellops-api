49cc914b9815489d5ac8c4df11db2f7336a67b75
Mohsen Esmaeili <m-esm@hotmail.com>
2021-01-31 23:47:55 +0330

    test: fix api guard test

--- src/api.guard.spec.ts
+++ src/api.guard.spec.ts
@@ -1,7 +1,10 @@
 import { ApiGuard } from './api.guard';
+import { DatabaseService } from './database/database.service';
+import { LoggerService } from './database/logger.service';
+import { SysinfoService } from './sysinfo/sysinfo.service';
 
 describe('ApiGuard', () => {
   it('should be defined', () => {
-    expect(new ApiGuard()).toBeDefined();
+    expect(new ApiGuard(new DatabaseService(), new SysinfoService(),new LoggerService())).toBeDefined();
   });
 });
