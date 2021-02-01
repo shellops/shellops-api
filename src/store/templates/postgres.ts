de1d9c2a41367c6f9556b071bdaa1326d3122573
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-01 04:44:25 +0330

    fix: postgres version

--- src/store/templates/postgres.ts
+++ src/store/templates/postgres.ts
@@ -2,13 +2,17 @@ import { AppTemplate } from '../app-template.dto';
 
 export const postgres = {
   name: 'PostgreSQL',
-  description:'PostgreSQL, often simply "Postgres", is an object-relational database management system (ORDBMS) with an emphasis on extensibility and standards-compliance.',
-  image: 'postgres:latest',
+  description:
+    'PostgreSQL, often simply "Postgres", is an object-relational database management system (ORDBMS) with an emphasis on extensibility and standards-compliance.',
+  image: 'postgres:9.6',
+  version: '9.6',
   logo: '/assets/logos/postgresql-original.svg',
   ports: [{ host: '{{ RANDOM_PORT }}', container: '5432' }],
-  variables: [{
-    name:'POSTGRES_PASSWORD',
-    description: 'Root password for PostgreSQL instance',
-    value: '{{ RANDOM_PASSWORD }}'
-  }]
+  variables: [
+    {
+      name: 'POSTGRES_PASSWORD',
+      description: 'Root password for PostgreSQL instance',
+      value: '{{ RANDOM_PASSWORD }}',
+    },
+  ],
 } as AppTemplate;
