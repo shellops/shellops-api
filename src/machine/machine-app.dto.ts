984a3c975719621df29f30730f79960c67ce9aeb
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-01 04:26:03 +0330

    feat: render machine templates

--- src/machine/machine-app.dto.ts
+++ src/machine/machine-app.dto.ts
@@ -1,10 +1,26 @@
+import { v4 } from 'uuid';
 import { AppTemplate } from '../store/app-template.dto';
-
+import * as mustache from 'mustache';
+import * as _ from 'lodash';
+import { randomBytes } from 'crypto';
 export class MachineApp extends AppTemplate {
   constructor(public id: string, template: Partial<AppTemplate>) {
     super();
-    Object.assign(this, template);
+
+    const rendered = mustache.render(JSON.stringify(template), {
+      RANDOM_PORT: () => _.random(10000, 60000, false),
+      RANDOM_PASSWORD: () => randomBytes(24).toString('hex'),
+      RANDOM_USERNAME: () => randomBytes(6).toString('hex'),
+    });
+
+    Object.assign(this, JSON.parse(rendered));
+
+    this.container = `${this.name.toLowerCase().replace(/\s/g, '-')}_${
+      id || v4()
+    }`;
   }
+
+  container: string;
 
   domains?: string[];
 
