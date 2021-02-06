66288311c7074dde09a74e2f5a767f87e7425aaf
Hasan Parasteh <hasanparasteh@gmail.com>
2021-02-06 09:36:10 +0330

    refactor: removes duplicate /api from controllers

--- src/machine/machine.controller.ts
+++ src/machine/machine.controller.ts
@@ -21,49 +21,49 @@ import { MachineService } from './machine.service';
 export class MachineController {
   constructor(private readonly machineService: MachineService) {}
 
-  @Get('/api/v1/machine/apps')
+  @Get('v1/machine/apps')
   @ApiBody({ type: MachineApp, isArray: true })
   getApps() {
     return this.machineService.getApps();
   }
 
-  @Post('/api/v1/machine/apps')
+  @Post('v1/machine/apps')
   @ApiBody({ type: AppTemplate })
   @ApiResponse({ type: MachineApp, isArray: true })
   installApp(@Body() template: AppTemplate) {
     return this.machineService.installApp(template);
   }
 
-  @Delete('/api/v1/machine/apps')
+  @Delete('v1/machine/apps')
   uninstallApps() {
     return this.machineService.uninstallApps();
   }
 
-  @Get('/api/v1/machine/apps/:appId')
+  @Get('v1/machine/apps/:appId')
   @ApiBody({ type: MachineApp, isArray: true })
   @ApiParam({ name: 'appId' })
   getApp(@Param('appId') appId: string) {
     return this.machineService.getApp(appId);
   }
 
-  @Delete('/api/v1/machine/apps/:appId')
+  @Delete('v1/machine/apps/:appId')
   @ApiBody({ type: MachineApp, isArray: true })
   @ApiParam({ name: 'appId' })
   uninstallApp(@Param('appId') appId: string) {
     return this.machineService.getApp(appId);
   }
 
-  @Post('/api/v1/machine/apps/:appId/start')
+  @Post('v1/machine/apps/:appId/start')
   startApp(@Param('appId') appId: string) {
     return this.machineService.startApp(appId);
   }
 
-  @Post('/api/v1/machine/apps/:appId/stop')
+  @Post('v1/machine/apps/:appId/stop')
   stopApp(@Param('appId') appId: string) {
     return this.machineService.startApp(appId);
   }
 
-  @Post('/api/v1/machine/apps/:appId/restart')
+  @Post('v1/machine/apps/:appId/restart')
   restartApp(@Param('appId') appId: string) {
     return this.machineService.restartApp(appId);
   }
