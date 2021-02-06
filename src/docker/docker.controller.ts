38e78324da9a6a672cba5adbffa28cf0090c518d
Hasan Parasteh <42239446+hasanparasteh@users.noreply.github.com>
2021-02-06 07:06:54 +0000

    refactor: #21 PR remaining /api in codes

--- src/docker/docker.controller.ts
+++ src/docker/docker.controller.ts
@@ -12,35 +12,35 @@ export class DockerController {
   constructor(private readonly dockerService: DockerService) {}
 
 
-  @Get('/api/v1/docker/containers')
+  @Get('v1/docker/containers')
   list() {
     return this.dockerService.containers();
   }
 
   
-  @Get('/api/v1/docker/containers/:containerId')
+  @Get('v1/docker/containers/:containerId')
   @ApiParam({ name: 'containerId' })
   inspect(@Param('containerId') containerId: string) {
     return this.dockerService.containerInfo(containerId);
   }
 
-  @Delete('/api/v1/docker/containers/:containerId')
+  @Delete('v1/docker/containers/:containerId')
   @ApiParam({ name: 'containerId' })
   remove(@Param('containerId') containerId: string) {
     return this.dockerService.removeContainer(containerId);
   }
 
-  @Post('/api/v1/docker/containers/:containerId/start')
+  @Post('v1/docker/containers/:containerId/start')
   start(@Param('containerId') containerId: string) {
     return this.dockerService.startContainer(containerId);
   }
 
-  @Post('/api/v1/docker/containers/:containerId/stop')
+  @Post('v1/docker/containers/:containerId/stop')
   stop(@Param('containerId') containerId: string) {
     return this.dockerService.stopContainer(containerId);
   }
 
-  @Post('/api/v1/docker/containers/:containerId/restart')
+  @Post('v1/docker/containers/:containerId/restart')
   restart(@Param('containerId') containerId: string) {
     return this.dockerService.restartContainer(containerId);
   }
