3a44d6405bbe975b921a1c05f40f3aa7a5f47d50
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-12 21:23:28 +0330

    fix: remove api guard log

--- src/api.guard.ts
+++ src/api.guard.ts
@@ -21,50 +21,21 @@ import { toUpper } from 'lodash';
  * Validates access from panel to remote nodes
  */
 @Injectable()
-export class ApiGuard implements CanActivate, OnApplicationBootstrap {
+export class ApiGuard implements CanActivate {
   constructor(
     private readonly databaseService: DatabaseService,
     private readonly sysInfoService: SysinfoService,
     private readonly loggerService: LoggerService,
-  ) {}
+  ) { }
 
-  private async getCredentials() {
+  public async getCredentials() {
     return this.databaseService.get<{
       user: string;
       pass: string;
     }>('agent-auth');
   }
-  async onApplicationBootstrap() {
-    let auth = await this.getCredentials();
 
-    const publicIp = await this.sysInfoService.ip();
-
-    if (!auth) {
-      auth = {
-        user: randomBytes(6).toString('hex'),
-        pass: randomBytes(24).toString('hex'),
-      };
-      await this.databaseService.update('agent-auth', auth);
-    }
-
-    this.loggerService.verbose(
-      chalk.bold.greenBright
-        .bgBlack`\n\nUse following url to add your host on shellops.io/panel\n\n\t` +
-        chalk.underline
-          .yellow`http://${auth.user}:${auth.pass}@${publicIp}:${Config.port}\n` +
-        (Config.env === ENV.DEVELOPMENT
-          ? `\n\t\tOR\n\n` +
-            chalk.underline
-              .yellow`\thttp://${auth.user}:${auth.pass}@localhost:${Config.port}\n`
-          : '') +
-          chalk.bgBlack.greenBright`\n\tUSER: ` +
-          chalk.yellow`${auth.user}` +
-          chalk.bgBlack.greenBright`\n\tPASS: ` +
-          chalk.yellow`${auth.pass}` ,
-    );
-  }
-
-  reject(res: Response) {
+  private reject(res: Response) {
     res.statusCode = 401;
     res.setHeader('WWW-Authenticate', 'Basic realm="example"');
   }
