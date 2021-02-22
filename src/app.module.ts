327745650edc0299fa2b1a1a11d54dbb6ee56df2
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-22 04:39:15 +0330

    feat: use global log and filter

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AppController } from './app.controller';
+import { AppService } from './app/app.service';
 import { DatabaseModule } from './database/database.module';
 import { DockerModule } from './docker/docker.module';
 import { FinancialModule } from './financial/financial.module';
+import { HttpExceptionsFilter } from './http.exception.filter';
+import { LoggingInterceptor } from './logging.interceptor';
 import { MachineModule } from './machine/machine.module';
+import { ShellService } from './shell/shell.service';
 import { StoreModule } from './store/store.module';
 import { SysinfoModule } from './sysinfo/sysinfo.module';
 import { WsModule } from './ws/ws.module';
-import { AppService } from './app/app.service';
-import { ShellService } from './shell/shell.service';
 
 @Module({
   imports: [
@@ -22,6 +25,9 @@ import { ShellService } from './shell/shell.service';
     DatabaseModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ShellService ],
+  providers: [AppService, ShellService,
+    { provide: APP_INTERCEPTOR, useClass: LoggingInterceptor },
+    { provide: APP_FILTER, useClass: HttpExceptionsFilter }
+  ],
 })
 export class AppModule { }
