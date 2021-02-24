4224b1ef95e9122a96f72d77067a1b0d61b05824
Mohsen Esmaeili <m-esm@hotmail.com>
2021-02-24 05:30:55 +0330

    fix: redis image

--- src/store/templates/redis.ts
+++ src/store/templates/redis.ts
@@ -2,10 +2,10 @@ import { AppTemplate } from '../app-template.dto';
 
 export const redis = {
   name: 'Redis',
-  version: '6.0',
+  version: '6.2.0',
   description:
     'Redis is an open-source, networked, in-memory, key-value data store with optional durability. It is written in ANSI C. The development of Redis is sponsored by Redis Labs today.',
-  image: 'mongo:6.0',
+  image: 'redis',
   logo: '/logos/redis-original.svg',
   ports: [{ host: '{{ RANDOM_PORT }}', container: '6379' }],
   variables: [
